ec36b7763aeb3622b30982df7ef31d44c9fa4d2f
DuncanZhou <364451395@qq.com>
2017-12-05 21:41:49 +0800

    字符串匹配

---
+++ question1082/Main.java
@@ -0,0 +1,16 @@
+package question1082;
+
+import java.util.Scanner;
+
+/**
+ * Created by duncan on 17-12-5.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+            String str = in.nextLine();
+            System.out.println(str.replaceAll("(?i)marshtomp","fjxmlhx"));
+        }
+    }
+}
