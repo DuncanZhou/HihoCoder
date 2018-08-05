87e38763a864ddf688efdb89239c87f79dc72de0
DuncanZhou <364451395@qq.com>
2018-08-05 13:10:21 +0800

    Pattern - Matcher - find()

--- SortingPhotoFiles/Main.java
+++ SortingPhotoFiles/Main.java
@@ -2,11 +2,14 @@ package SortingPhotoFiles;
 
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 class node implements Comparable<node>{
     public String string;
     public int val;
+    //构造函数
     public node(String s,int i){
         string = s;
         val = i;
@@ -22,15 +25,17 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         node[] nodes = new node[n];
-//        Pattern p = Pattern.compile("(a-zA-Z)(0-9)");
+        //正则匹配表达式
+        Pattern p = Pattern.compile("([a-zA-Z]+)([0-9]+)");
         for(int i = 0; i < n; i++) {
             String temp = in.next();
-//            Matcher m = p.matcher(temp);
-//            String[] substrings = temp.split("(a-zA-Z)(0-9)");
-            String t_s = temp.replaceAll("[^(a-zA-Z)]","");
-            int t_v = Integer.valueOf(temp.replaceAll("[^(0-9)]",""));
-            node t_node = new node(t_s,t_v);
-            nodes[i] = t_node;
+            //匹配
+            Matcher m = p.matcher(temp);
+            //find()要加
+           if(m.find()) {
+               node t_node = new node(m.group(1), Integer.valueOf(m.group(2)));
+               nodes[i] = t_node;
+           }
         }
         Arrays.sort(nodes);
         for(int i = 0; i < nodes.length; i++)
